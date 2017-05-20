6fac2735d18db2365a05e1fe823f9c5df9b908e2
givebest <givenlovs@msn.com>
2017-05-20 17:36:29 +0800

    完成

---
+++ README.md
@@ -0,0 +1,56 @@
+# GB-respond
+----
+
+## 简介
+
+移动端响应式布局解决方案、用 `JavaScript` 根据屏幕宽度计算 `Root` 字体大小弥补媒体查询断点断层过大问题，一切元素单位皆使用 `REM`。
+  
+
+## 演示
+
+[http://givebest.github.io/gb-respond.html](http://givebest.github.io/gb-respond.html)
+
+## 使用
+
+###  HTML
+
+> 在 Head 引入（CSS前面）
+	
+```html	
+<head>
+  <!-- 省略 -->
+  <script src="js/GB-respond.js"></script>
+  <link rel="stylesheet" href="css/normalize.css">
+</head>
+```
+
+### CSS
+
+> 以 375 （设计稿 750 * N） 屏幕宽度为基础，推算出元素相对应的 REM 值，如设计稿字体为：40px，那么换算下来为：40 / 2 / 100 = .2rem
+
+```css
+
+html {
+  font-size: 100px;
+}
+
+body {
+	font-size: .12rem; /* 在 375 屏幕宽相当于 12px */
+}
+
+```
+
+
+
+
+## 感谢他们
+
+参考： [https://github.com/amfe/lib-flexible](https://github.com/amfe/lib-flexible)       
+
+
+
+## License
+
+[MIT](./LICENSE) © 2017 [givebest](https://github.com/givebest)
+
+ 
---
+++ src/js/GB-respond.js
@@ -0,0 +1,49 @@
+/**
+* GB-respond.js
+* @see https://github.com/givebest/GB-respond
+* @author givenlovs[at]msn.com
+* @(c) 2017
+**/
+;(function (win) {
+  var doc = win.document,
+    docEl = doc.documentElement,
+    designWidth = 750 / 2,   // 设计稿宽度
+    maxWidth = 1280 / 2;   // 最大支持宽度（无限制会导致 Pad 等大屏设备展示内容过少、图像失真等）
+
+  win.addEventListener('resize', function() {
+    throttle(refreshRem);
+  }, false);
+
+  // @see https://github.com/amfe/lib-flexible/blob/master/src/flexible.js
+  win.addEventListener('pageshow', function (e) {
+    if (e.persisted) {
+      throttle(refreshRem);
+    }
+  }, false);
+
+  // 初始化
+  throttle(refreshRem);
+
+  /**
+   * 计算字体大小
+   * @return {[type]} [description]
+   */
+  function refreshRem () {
+    var width = docEl.getBoundingClientRect().width || docEl.clientWidth,
+      fontSize = 100 / designWidth * (width > maxWidth ? maxWidth : width) + 'px';
+      docEl.style.fontSize = fontSize;
+  }
+
+  /**
+   * 节流函数
+   * @param  {[type]} method  [description]
+   * @param  {[type]} context [description]
+   * @return {[type]}         [description]
+   */
+  function throttle (method, context) {
+    clearTimeout(method.tId);
+    method.tId = setTimeout (function () {
+      method.call(context);
+    }, 100);
+  }
+})(window);
